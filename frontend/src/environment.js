51bef200ca297383f95b338cf72f1712b3427d9f
sivaUma964065 <136900528+sivaUma964065@users.noreply.github.com>
2026-09-13 22:59:37 +0530

    Initial Commit

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,4 @@
-const server = "https://connectly-backend-s766.onrender.com";
-// const server="http://localhost:3002";
+//const server = "https://connectly-backend-s766.onrender.com";
+const server="http://localhost:3002";
 
 export default server;
